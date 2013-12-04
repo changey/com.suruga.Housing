13e70400bb4e4886c58e25a409030b181b8226b7
Eric Chang <changey@mit.edu>
2013-12-04 14:54:39 -0500

    clone

--- com.suruga.Housing/FirstViewController.m
+++ com.suruga.Housing/FirstViewController.m
@@ -3,7 +3,7 @@
 //  com.suruga.Housing
 //
 //  Created by yasho on 11/22/13.
-//  Copyright (c) 2013 MIT. All rights reserved.
+//  Copyright (c) 2013 MIT. All rights reserved. 
 //
 
 #import "FirstViewController.h"
